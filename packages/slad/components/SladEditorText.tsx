6700ec1824a05cfe5723222e62c11e89f526781a
Daniel Steigerwald <daniel@steigerwald.cz>
2019-08-08 03:37:26 +0200

    Refactor SladEditorText

--- packages/slad/components/SladEditorText.tsx
+++ packages/slad/components/SladEditorText.tsx
@@ -1,19 +1,19 @@
-import React, { PureComponent, useCallback, useMemo } from 'react';
+import React, { Component, useCallback, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { SladPath } from './SladEditorSetNodePathContext';
 import { useSladEditorSetNodePathRef } from '../hooks/useSladEditorSetNodePathRef';
 import { useMemoPath } from '../hooks/useMemoPath';
 
-// Yes, we use PureComponent and findDOMNode because it's the only way
-// how we can retrieve TextNode reference via React.
-// Otherwise, we would have to traverse DOM which is slower and error-prone.
-// We don't want to wrap text with span because that would mess DOM.
+// Yes, we use Component and findDOMNode because it's the only way how
+// we can get TextNode DOM reference with React.
+// Otherwise, we would have to traverse DOM which would be slower and error-prone.
+// We don't want to wrap text because that would mess DOM.
 // https://github.com/facebook/react/blob/master/packages/react-dom/src/__tests__/ReactDOMFiber-test.js#L92
 // Check: 'finds the DOM Text node of a string child'
-class TextComponent extends PureComponent<{ text: string }> {
+class TextNode extends Component<{ value: string }> {
   render() {
-    const { text } = this.props;
-    return text;
+    const { value } = this.props;
+    return value;
   }
 }
 
@@ -35,7 +35,7 @@ export function SladEditorText({
   const sladEditorSetNodePathRef = useSladEditorSetNodePathRef(memoPath);
 
   const handleRef = useCallback(
-    (instance: TextComponent) => {
+    (instance: TextNode) => {
       // We know it can return Text or null only.
       // eslint-disable-next-line react/no-find-dom-node
       const textNode = ReactDOM.findDOMNode(instance) as Text | null;
@@ -45,6 +45,6 @@ export function SladEditorText({
   );
 
   return useMemo(() => {
-    return <TextComponent text={text.value} ref={handleRef} />;
+    return <TextNode value={text.value} ref={handleRef} />;
   }, [handleRef, text.value]);
 }
