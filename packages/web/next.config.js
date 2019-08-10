e912162014a19ab0ebe8cacb0961fb98c15b630d
Daniel Steigerwald <daniel@steigerwald.cz>
2019-08-10 17:13:06 +0200

    Add next-bundle-analyzer

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,8 +1,23 @@
 const withTranspileModules = require('next-transpile-modules');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-module.exports = withTranspileModules({
-  // To force Next.js to transpile code from other workspace packages.
-  // https://github.com/martpie/next-transpile-modules
-  transpileModules: ['slad'],
-  target: 'serverless',
-});
+module.exports = withBundleAnalyzer(
+  withTranspileModules({
+    // To force Next.js to transpile code from other workspace packages.
+    // https://github.com/martpie/next-transpile-modules
+    transpileModules: ['slad'],
+    target: 'serverless',
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html',
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html',
+      },
+    },
+  }),
+);
