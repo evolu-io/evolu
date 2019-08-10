38b7bf7b0799641dfc7eef83d921071ddee592b4
Daniel Steigerwald <daniel@steigerwald.cz>
2019-08-10 18:46:29 +0200

    Update SladEditorRenderElementContext.ts
    Add jsdoc comment

--- packages/slad/components/SladEditorRenderElementContext.ts
+++ packages/slad/components/SladEditorRenderElementContext.ts
@@ -1,6 +1,10 @@
 import { createContext, ReactNode } from 'react';
 import { SladEditorSetNodePathRef } from '../hooks/useSladEditorSetNodePathRef';
 
+/**
+ * SladElement is the most general base interface from which all interfaces inherit.
+ * It only has properties common to all kinds of elements.
+ */
 export interface SladElement {
   readonly children?: readonly (SladElement | string)[] | null;
 }
