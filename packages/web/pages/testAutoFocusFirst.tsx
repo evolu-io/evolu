a15b7139ee301b1447f14337d3022b5f6485c995
Daniel Steigerwald <daniel@steigerwald.cz>
2019-08-23 22:04:04 +0200

    Add test for focus after window blur

--- packages/web/pages/testAutoFocusFirst.tsx
+++ packages/web/pages/testAutoFocusFirst.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BasicExample } from '../components/examples/BasicExample';
 import { SchemaExample } from '../components/examples/SchemaExample';
 
 function TestAutoFocusFirst() {
+  useEffect(() => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-alert, no-undef
+      window.alert('foo');
+    }, 50);
+  }, []);
   return (
     <>
       <BasicExample autoFocus />
--- packages/web/tests/integration/testAutoFocusFirst.test.ts
+++ packages/web/tests/integration/testAutoFocusFirst.test.ts
@@ -2,12 +2,19 @@ import { pageUrl } from './helpers/pageUrl';
 import { pageDom } from './helpers/pageDom';
 
 beforeEach(async () => {
+  page.on('dialog', async dialog => {
+    await dialog.dismiss();
+  });
   await page.goto(pageUrl('testAutoFocusFirst'));
   await page.waitFor(50);
 });
 
 test('render', async () => {
   await expect(await pageDom()).toMatchSnapshot();
+  // TODO: Figure out how to test focus is not lost on alert.
+  // Manual test for now:
+  // Open http://localhost:3000/testAutoFocusFirst, close alert,
+  // check if editor still has a focus.
 });
 
 // https://github.com/steida/slad/issues/14
