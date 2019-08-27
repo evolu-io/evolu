44b2f727daf625ebfbbe4f85c3ea6165f66eb2ba
Daniel Steigerwald <daniel@steigerwald.cz>
2019-08-27 03:31:11 +0200

    Update testRenderer.tsx
    Yep, it's super minimal. No dependencies. Just render.

--- packages/web/pages/testRenderer.tsx
+++ packages/web/pages/testRenderer.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Renderer } from 'slad';
-import { initialBasicElement } from '../components/examples/BasicExample';
+import { Renderer, EditorReactDOMElement } from 'slad';
 // import {
 //   initialSchemaRootElement,
 //   useSchemaRenderElement,
 // } from '../components/examples/SchemaExample';
 
+export const initialEditorReactDOMElement: EditorReactDOMElement = {
+  children: [
+    {
+      props: {
+        style: { fontSize: '24px' },
+      },
+      children: ['heading'],
+    },
+    {
+      props: {
+        style: { fontSize: '16px' },
+      },
+      children: ['paragraph'],
+    },
+  ],
+};
+
 function TestRenderer() {
   return (
     <div>
-      <Renderer element={initialBasicElement} />
+      <Renderer element={initialEditorReactDOMElement} />
       {/* <Renderer
         element={initialSchemaRootElement}
         renderElement={useSchemaRenderElement}
