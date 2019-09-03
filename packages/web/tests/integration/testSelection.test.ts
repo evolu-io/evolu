03e9c0aa2473ba045373ba7f9a3b9421a06e390f
Daniel Steigerwald <daniel@steigerwald.cz>
2019-09-03 15:12:52 +0200

    Yep, for testSelection

--- packages/web/tests/integration/testSelection.test.ts
+++ packages/web/tests/integration/testSelection.test.ts
@@ -9,19 +9,24 @@ test('select', async () => {
   await expect(await pageDom()).toMatchSnapshot();
 
   await page.click('.select-first-two-letters');
+  await page.waitFor(50);
   await expect(await pageDom()).toMatchSnapshot();
 
   await page.click('.select-first-two-letters-backward');
+  await page.waitFor(50);
   await expect(await pageDom()).toMatchSnapshot();
 
   await page.keyboard.down('Shift');
   await page.keyboard.press('ArrowRight');
+  await page.waitFor(50);
   await expect(await pageDom()).toMatchSnapshot();
 
   await page.click('.select-all');
+  await page.waitFor(50);
   await expect(await pageDom()).toMatchSnapshot();
 
   await page.click('.select-all-backward');
+  await page.waitFor(50);
   await expect(await pageDom()).toMatchSnapshot();
 
   // TODO: Once we decide what we want.
