4bb61d7db46e2870531aa9fbbfde54f6df245f21
Daniel Steigerwald <daniel@steigerwald.cz>
2019-09-09 03:00:48 +0200

    Make snapshots more readable and useLogEditorState faster

--- packages/slad/hooks/useLogEditorState.tsx
+++ packages/slad/hooks/useLogEditorState.tsx
@@ -1,12 +1,36 @@
-import React, {
-  useCallback,
-  ReactNode,
-  useState,
-  useMemo,
-  Fragment,
-} from 'react';
+import React, { useCallback, ReactNode, useState, useMemo, memo } from 'react';
 import { EditorState } from '../components/Editor';
 import { EditorElement, recursiveRemoveID } from '../models/element';
+
+const Item = memo(
+  <T extends EditorElement>({
+    editorState,
+    index,
+  }: {
+    editorState: EditorState<T>;
+    index: number;
+  }) => {
+    // hasFocus render first
+    const { element, hasFocus, ...rest } = editorState;
+    // Deliberately do not prettify. Smaller output is more readable in snapshots.
+    // No IDs because that would break integration tests.
+    const title = JSON.stringify(recursiveRemoveID(element))
+      .split('"')
+      .join("'");
+    return (
+      <span title={title}>
+        {index} {JSON.stringify({ hasFocus, ...rest })}
+        <style jsx>{`
+          span {
+            color: #888;
+            display: block;
+            line-height: 24px;
+          }
+        `}</style>
+      </span>
+    );
+  },
+);
 
 export function useLogEditorState<T extends EditorElement>(
   editorState: EditorState<T>,
@@ -20,27 +44,23 @@ export function useLogEditorState<T extends EditorElement>(
 
   const logEditorStateElement = useMemo(() => {
     return (
-      <pre style={{ height: 48, overflow: 'auto' }}>
+      <pre>
         {editorStates
           .map((editorState, index) => {
-            // hasFocus, to render it first
-            const { element, hasFocus, ...rest } = editorState;
-            // Deliberately do not prettify. Smaller output is more readable in snapshots.
-            const title = JSON.stringify(recursiveRemoveID(element))
-              .split('"')
-              .join("'");
-            return (
-              // eslint-disable-next-line react/no-array-index-key
-              <Fragment key={index}>
-                <span title={title} style={{ color: '#888' }}>
-                  {index} {JSON.stringify({ hasFocus, ...rest })}
-                </span>
-                <br />
-                <br />
-              </Fragment>
-            );
+            // Key index is ok, because the order is stable.
+            // eslint-disable-next-line react/no-array-index-key
+            return <Item editorState={editorState} index={index} key={index} />;
           })
           .reverse()}
+        <style jsx>{`
+          pre {
+            height: 48px;
+            overflow: auto;
+          }
+          span {
+            color: #888;
+          }
+        `}</style>
       </pre>
     );
   }, [editorStates]);
