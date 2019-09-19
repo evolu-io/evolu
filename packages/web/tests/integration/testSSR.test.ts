c09c928597656ced7124e30ed49b5df887073549
Daniel Steigerwald <daniel@steigerwald.cz>
2019-09-19 03:14:22 +0200

    Fix CI

--- packages/web/tests/integration/testSSR.test.ts
+++ packages/web/tests/integration/testSSR.test.ts
@@ -1,6 +1,5 @@
 import snapshotDiff from 'snapshot-diff';
-
-import { pageDom, pageUrl } from './helpers';
+import { pageDom, pageUrl, pageAwaitFor50ms } from './helpers';
 
 test('dom before JS is executed', async () => {
   await page.goto(pageUrl('testSSR'), { waitUntil: 'domcontentloaded' });
@@ -9,6 +8,7 @@ test('dom before JS is executed', async () => {
 
 test('dom after JS is executed', async () => {
   await page.goto(pageUrl('testSSR'));
+  await pageAwaitFor50ms();
   await expect(await pageDom()).toMatchSnapshot();
 });
 
@@ -16,6 +16,7 @@ test('dom diff before and after JS is executed', async () => {
   await page.goto(pageUrl('testSSR'), { waitUntil: 'domcontentloaded' });
   const before = await pageDom();
   await page.goto(pageUrl('testSSR'));
+  await pageAwaitFor50ms();
   const after = await pageDom();
   await expect(snapshotDiff(before, after)).toMatchSnapshot();
 });
