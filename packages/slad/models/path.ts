22266dca7af7c5b92cf2c67fc63ad1d6e85d14e3
Daniel Steigerwald <daniel@steigerwald.cz>
2019-10-10 04:23:42 +0200

    Put back invariant for empty editor path
    It will be replaced with Option later.

--- packages/slad/models/path.ts
+++ packages/slad/models/path.ts
@@ -1,8 +1,16 @@
-import { Endomorphism } from 'fp-ts/lib/function';
+import { Endomorphism, Predicate } from 'fp-ts/lib/function';
 import { eqNumber } from 'fp-ts/lib/Eq';
 import { getEq } from 'fp-ts/lib/Array';
+import invariant from 'tiny-invariant';
 
 export type EditorPath = number[];
+
+export const editorPathIsEmpty: Predicate<EditorPath> = path =>
+  path.length === 0;
+
+export function invariantEditorPathIsEmpty(path: EditorPath) {
+  invariant(!editorPathIsEmpty(path), 'EditorPath is empty.');
+}
 
 export type NodesEditorPathsMap = Map<Node, EditorPath>;
 
@@ -13,18 +21,23 @@ export type EditorPathsNodesMap = Map<string, Node>;
 
 export const eqEditorPath = getEq(eqNumber);
 
+// TODO: Consider Option for getParentPath and getParentPathAndLastIndex.
+
 /**
  * Example: `[0, 1, 2]` to `[0, 1]`.
  */
 export function getParentPath(path: EditorPath): EditorPath {
+  invariantEditorPathIsEmpty(path);
   return path.slice(0, -1) as EditorPath;
 }
 
 /**
  * Example: `[0, 1, 2]` to `[[0, 1], 2]`.
- * TODO: rename na get!
  */
-export function parentPathAndLastIndex(path: EditorPath): [EditorPath, number] {
+export function getParentPathAndLastIndex(
+  path: EditorPath,
+): [EditorPath, number] {
+  invariantEditorPathIsEmpty(path);
   return [getParentPath(path), path[path.length - 1]];
 }
 
@@ -37,7 +50,7 @@ export function editorPathsAreForward(
 
 export function movePath(offset: number): Endomorphism<EditorPath> {
   return path => {
-    const [parentPath, lastIndex] = parentPathAndLastIndex(path);
+    const [parentPath, lastIndex] = getParentPathAndLastIndex(path);
     return [...parentPath, lastIndex + offset] as EditorPath;
   };
 }
