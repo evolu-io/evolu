e22c2859cfa5065a1246f4d75e8569150bc08886
Daniel Steigerwald <daniel@steigerwald.cz>
2019-10-22 01:33:07 +0200

    Explain Range usage

--- packages/evolu/models/range.ts
+++ packages/evolu/models/range.ts
@@ -2,6 +2,8 @@ import { Path } from './path';
 
 /**
  * Like DOM Range, but with EditorPath for the start and the end.
+ * Range should be an implementation detail where an operation needs to know
+ * the direction.
  * https://developer.mozilla.org/en-US/docs/Web/API/Range
  */
 export interface Range {
