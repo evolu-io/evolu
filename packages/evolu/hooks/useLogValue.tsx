b684bd06d6000d28519a06454d71c065bed8bd6b
Daniel Steigerwald <daniel@steigerwald.cz>
2019-10-27 01:49:00 +0200

    Improve useLogValue

--- packages/evolu/hooks/useLogValue.tsx
+++ packages/evolu/hooks/useLogValue.tsx
@@ -5,52 +5,69 @@ import { toUndefined } from 'fp-ts/lib/Option';
 import { mapElementToIDless } from '../models/element';
 import { Value } from '../types';
 
-const Item = memo(({ value, index }: { value: Value; index: number }) => {
-  const { element, hasFocus, selection } = value;
-  const indexItem = { hasFocus, selection: toUndefined(selection) };
-  // Deliberately do not prettify. Smaller output is more readable in snapshots.
-  // No IDs because that would break integration tests.
-  const elementTextForSnapshots = JSON.stringify(mapElementToIDless(element))
-    .split('"')
-    .join("'");
-  const [shown, setShow] = useState(false);
-  const handleItemMouseDown = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      e.preventDefault();
-      setShow(!shown);
-    },
-    [shown],
-  );
-  return (
-    <span onMouseDown={handleItemMouseDown} data-json={elementTextForSnapshots}>
-      {index} {JSON.stringify(indexItem)}
-      {shown && (
-        <div title="click to close" onMouseDown={handleItemMouseDown}>
-          {JSON.stringify(mapElementToIDless(element), null, 2)}
-        </div>
-      )}
-      <style jsx>{`
-        span {
-          color: #888;
-          display: block;
-          line-height: 24px;
-          cursor: pointer;
-        }
-        div {
-          position: absolute;
-          left: 0;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          z-index: 10;
-          line-height: 16px;
-          background-color: #fff;
-          padding: 16px;
-        }
-      `}</style>
-    </span>
-  );
-});
+const Item = memo(
+  ({
+    value,
+    index,
+    selectedIndex,
+    onSelect,
+  }: {
+    value: Value;
+    index: number;
+    selectedIndex: number | null;
+    onSelect: (index: number) => void;
+  }) => {
+    const { element, hasFocus, selection } = value;
+    const indexItem = { hasFocus, selection: toUndefined(selection) };
+    // Deliberately do not prettify. Smaller output is more readable in snapshots.
+    // No IDs because that would break integration tests.
+    const elementTextForSnapshots = JSON.stringify(mapElementToIDless(element))
+      .split('"')
+      .join("'");
+    const shown = index === selectedIndex;
+    const handleItemMouseDown = useCallback(
+      (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.preventDefault();
+        onSelect(index);
+      },
+      [onSelect, index],
+    );
+    const text = `${index} ${JSON.stringify(indexItem)}`.split('"').join("'");
+    return (
+      <span
+        onMouseDown={handleItemMouseDown}
+        data-json={elementTextForSnapshots}
+      >
+        {shown ? <b>{text}</b> : text}
+        {shown && (
+          <div title="click to close" onMouseDown={handleItemMouseDown}>
+            {JSON.stringify(mapElementToIDless(element), null, 2)}
+          </div>
+        )}
+        <style jsx>{`
+          span {
+            color: #888;
+            display: block;
+            line-height: 24px;
+            cursor: pointer;
+          }
+          div {
+            position: absolute;
+            left: 50%;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 10;
+            line-height: 16px;
+            background-color: #fff;
+            padding: 16px;
+            overflow: auto;
+          }
+        `}</style>
+      </span>
+    );
+  },
+);
 
 export const useLogValue = (
   value: Value,
@@ -59,15 +76,30 @@ export const useLogValue = (
   const logValue = useCallback((value: Value) => {
     setValues(prevValues => [...prevValues, value]);
   }, []);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const handleSelect = useCallback(
+    (index: number) => {
+      setSelectedIndex(index === selectedIndex ? null : index);
+    },
+    [selectedIndex],
+  );
 
   const logValueElement = useMemo(() => {
     return (
       <pre>
         {values
           .map((value, index) => {
-            // Key index is ok, because the order is stable.
-            // eslint-disable-next-line react/no-array-index-key
-            return <Item value={value} index={index} key={index} />;
+            return (
+              <Item
+                value={value}
+                index={index}
+                selectedIndex={selectedIndex}
+                onSelect={handleSelect}
+                // Key index is ok, because the order is stable.
+                // eslint-disable-next-line react/no-array-index-key
+                key={index}
+              />
+            );
           })
           .reverse()}
         <style jsx>{`
@@ -81,7 +113,7 @@ export const useLogValue = (
         `}</style>
       </pre>
     );
-  }, [values]);
+  }, [handleSelect, selectedIndex, values]);
 
   return [logValue, logValueElement];
 };
