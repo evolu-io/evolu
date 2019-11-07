d4fe94914e59e08cd5c0c0a78ac659385efc5277
Daniel Steigerwald <daniel@steigerwald.cz>
2019-11-07 02:06:42 +0100

    Refactor useBeforeInput

--- packages/evolu/hooks/useBeforeInput.ts
+++ packages/evolu/hooks/useBeforeInput.ts
@@ -1,5 +1,5 @@
 import { sequenceT } from 'fp-ts/lib/Apply';
-import { constTrue, constVoid } from 'fp-ts/lib/function';
+import { IO } from 'fp-ts/lib/IO';
 import {
   chain,
   filter,
@@ -8,10 +8,10 @@ import {
   Option,
   option,
   some,
-  toNullable,
 } from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { Dispatch, RefObject, useEffect } from 'react';
+import { constVoid } from 'fp-ts/lib/function';
 import {
   getDOMRangeFromInputEvent,
   getDOMSelection,
@@ -29,80 +29,90 @@ import { Action, AfterTyping, GetPathByDOMNode } from '../types';
 import { DOMText } from '../types/dom';
 import { warn } from '../warn';
 
+// We prevent everything except safe typing on the collapsed selection which can not
+// be prevented anyway, because browsers update DOM by an extensions or spellcheck
+// or by contentEditable itself (whitespaces can be replaced with nbsps).
+// In those cases, we read content from DOM then restore it so React is not confused.
+// We do not handle composition events yet.
+// https://www.w3.org/TR/input-events-2/
+const preventDefault = (event: InputEvent): IO<void> => () => {
+  event.preventDefault();
+};
+
 const rangeStartContainerToText: (a: Range) => Option<string> = ({
   startContainer,
 }) => fromNullable(startContainer.textContent);
 
-type TypingHandlerArg = {
-  getPathByDOMNode: GetPathByDOMNode;
-  event: InputEvent;
-  afterTyping: AfterTyping;
-  dispatch: Dispatch<Action>;
-};
+// TODO: Refactor pyramids of doom, afterTyping and dispatch should be functional.
 
-type PreventDefault = boolean;
-
-// Maybe we should handle typing handlers in Reducer, but I am not sure yet.
-export type TypingHandler = (arg: TypingHandlerArg) => PreventDefault;
-
-const insertTextOnTyping: TypingHandler = ({
-  getPathByDOMNode,
-  event,
-  afterTyping,
-  dispatch,
-}) =>
+const insertText = (
+  event: InputEvent,
+  afterTyping: AfterTyping,
+  dispatch: Dispatch<Action>,
+  doc: Document,
+  getPathByDOMNode: GetPathByDOMNode,
+) =>
   pipe(
-    sequenceT(option)(
-      getSelectionFromInputEvent(getPathByDOMNode, event)(),
-      getDOMRangeFromInputEvent(event),
-      fromNullable(event.data),
-    ),
-    chain(([selection, domRange, eventData]) => {
-      const maybeBR = domRange.startContainer.childNodes[domRange.startOffset];
-      const brIsGoingToBeReplacedWithText =
-        domRange.startContainer === domRange.endContainer &&
-        maybeBR != null &&
-        maybeBR.nodeName === 'BR';
-      const getText = () => domRange.startContainer.textContent || '';
-      const maybeReplaceBRWithText = () => {
-        if (!brIsGoingToBeReplacedWithText) return;
-        domRange.startContainer.replaceChild(maybeBR, domRange.startContainer
-          .firstChild as ChildNode);
-      };
-      const selectionAfterInsert = brIsGoingToBeReplacedWithText
-        ? snocSelection(selection, 1, 1)
-        : moveSelection(eventData.length)(selection);
-      return some({
-        getText,
-        maybeReplaceBRWithText,
-        selectionAfterInsert,
-      });
-    }),
-    fold(
-      constTrue,
-      ({ getText, maybeReplaceBRWithText, selectionAfterInsert }) => {
-        afterTyping(() => {
-          const text = getText();
-          maybeReplaceBRWithText();
-          dispatch({
-            type: 'insertText',
-            text,
-            selection: selectionAfterInsert,
+    getDOMSelection(doc)(),
+    fold(preventDefault(event), selection => {
+      if (!selection.isCollapsed) {
+        event.preventDefault();
+        return;
+      }
+      pipe(
+        sequenceT(option)(
+          getSelectionFromInputEvent(getPathByDOMNode, event)(),
+          getDOMRangeFromInputEvent(event),
+          fromNullable(event.data),
+        ),
+        chain(([selection, domRange, eventData]) => {
+          const maybeBR =
+            domRange.startContainer.childNodes[domRange.startOffset];
+          const brIsGoingToBeReplacedWithText =
+            domRange.startContainer === domRange.endContainer &&
+            maybeBR != null &&
+            maybeBR.nodeName === 'BR';
+          const getText = () => domRange.startContainer.textContent || '';
+          const maybeReplaceBRWithText = () => {
+            if (!brIsGoingToBeReplacedWithText) return;
+            domRange.startContainer.replaceChild(maybeBR, domRange
+              .startContainer.firstChild as ChildNode);
+          };
+          const selectionAfterInsert = brIsGoingToBeReplacedWithText
+            ? snocSelection(selection, 1, 1)
+            : moveSelection(eventData.length)(selection);
+          return some({
+            getText,
+            maybeReplaceBRWithText,
+            selectionAfterInsert,
           });
-        });
-        return false;
-      },
-    ),
+        }),
+        fold(
+          preventDefault(event),
+          ({ getText, maybeReplaceBRWithText, selectionAfterInsert }) => {
+            afterTyping(() => {
+              const text = getText();
+              maybeReplaceBRWithText();
+              dispatch({
+                type: 'insertText',
+                text,
+                selection: selectionAfterInsert,
+              });
+            });
+          },
+        ),
+      );
+    }),
   );
 
-const insertReplacementTextOnTyping: TypingHandler = ({
-  event,
-  afterTyping,
-  dispatch,
-}) =>
+const insertReplacementText = (
+  event: InputEvent,
+  afterTyping: AfterTyping,
+  dispatch: Dispatch<Action>,
+) =>
   pipe(
     getDOMRangeFromInputEvent(event),
-    fold(constTrue, range => {
+    fold(preventDefault(event), range => {
       afterTyping(() => {
         pipe(
           range,
@@ -112,51 +122,77 @@ const insertReplacementTextOnTyping: TypingHandler = ({
           }),
         );
       });
-      return false;
     }),
   );
 
-const deleteContentOnTyping: TypingHandler = ({
-  getPathByDOMNode,
-  event,
-  afterTyping,
-  dispatch,
-}) =>
+const deleteContent = (
+  event: InputEvent,
+  afterTyping: AfterTyping,
+  dispatch: Dispatch<Action>,
+  doc: Document,
+  getPathByDOMNode: GetPathByDOMNode,
+) => {
   pipe(
-    sequenceT(option)(
-      getSelectionFromInputEvent(getPathByDOMNode, event)(),
-      getDOMRangeFromInputEvent(event),
-    ),
-    fold(constTrue, ([selection, domRange]) => {
-      if (isCollapsed(selection)) return true;
-      const textIsGoingToBeReplacedWithBR =
-        domRange.startContainer === domRange.endContainer &&
-        domRange.startOffset === 0 &&
-        (domRange.startContainer as DOMText).data.length === domRange.endOffset;
-      if (textIsGoingToBeReplacedWithBR) event.preventDefault();
-      const getSelectionAfterDelete = () => {
-        if (!textIsGoingToBeReplacedWithBR)
-          return some(collapseToStart(selection));
-        return initSelection(selection);
-      };
-      pipe(
-        getSelectionAfterDelete(),
-        fold(
-          () => {
-            warn('Selection should exists.');
-          },
-          selection => {
-            afterTyping(() => {
-              const { data } = domRange.startContainer as DOMText;
-              const text = textIsGoingToBeReplacedWithBR ? '' : data;
-              dispatch({ type: 'deleteText', text, selection });
-            });
-          },
-        ),
-      );
-      return false;
+    getDOMSelection(doc)(),
+    filter(isExistingDOMSelection),
+    fold(preventDefault(event), selection => {
+      const onlyTextIsAffectedByAction =
+        selection.isCollapsed &&
+        // nodeValue != null for text node.
+        // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeValue
+        selection.anchorNode.nodeValue != null &&
+        selection.anchorOffset !==
+          (event.inputType === 'deleteContentBackward'
+            ? 0
+            : selection.anchorNode.nodeValue.length);
+      if (onlyTextIsAffectedByAction) {
+        pipe(
+          sequenceT(option)(
+            getSelectionFromInputEvent(getPathByDOMNode, event)(),
+            getDOMRangeFromInputEvent(event),
+          ),
+          fold(preventDefault(event), ([selection, domRange]) => {
+            if (isCollapsed(selection)) return;
+            const textIsGoingToBeReplacedWithBR =
+              domRange.startContainer === domRange.endContainer &&
+              domRange.startOffset === 0 &&
+              (domRange.startContainer as DOMText).data.length ===
+                domRange.endOffset;
+            if (textIsGoingToBeReplacedWithBR) event.preventDefault();
+            const getSelectionAfterDelete = () => {
+              if (!textIsGoingToBeReplacedWithBR)
+                return some(collapseToStart(selection));
+              return initSelection(selection);
+            };
+            pipe(
+              getSelectionAfterDelete(),
+              fold(
+                () => {
+                  warn('Selection should exists.');
+                },
+                selection => {
+                  afterTyping(() => {
+                    const { data } = domRange.startContainer as DOMText;
+                    const text = textIsGoingToBeReplacedWithBR ? '' : data;
+                    dispatch({ type: 'deleteText', text, selection });
+                  });
+                },
+              ),
+            );
+          }),
+        );
+      } else {
+        preventDefault(event)();
+        // pipe(
+        //   getSelectionFromInputEvent(getPathByDOMNode, event)(),
+        //   fold(constVoid, selection => {
+        //     dispatch({ type: 'deleteContent', selection });
+        //   }),
+        // );
+      }
     }),
   );
+};
 
 export const useBeforeInput = (
   editorElementRef: RefObject<HTMLDivElement>,
@@ -167,92 +203,30 @@ export const useBeforeInput = (
   useEffect(() => {
     const { current: editorElement } = editorElementRef;
     if (editorElement == null) return;
+    const doc = editorElement.ownerDocument;
+    if (doc == null) return;
 
     const handleBeforeInput = (event: InputEvent) => {
-      const domSelection = pipe(
-        fromNullable(editorElement.ownerDocument),
-        chain(doc => getDOMSelection(doc)()),
-        filter(isExistingDOMSelection),
-        toNullable,
-      );
-
-      // pipe switch?
-      // imho jo
-
-      if (domSelection == null) {
-        // TODO: Handle composition events.
-        warn('Selection should exists.');
-        return;
-      }
-
-      const arg: TypingHandlerArg = {
-        getPathByDOMNode,
-        event,
-        afterTyping,
-        dispatch,
-      };
-
-      // We prevent everything except typing on collapsed selection which can not
-      // be prevented, because we have to let browser to update DOM by an extension
-      // or spellcheck or by contentEditable itself (it replaces spaces with nbsps).
-      // In those cases, we read content from DOM then restore it so React is not
-      // confused. We do not handle composition events yet.
-      // https://www.w3.org/TR/input-events-2/
-      // nejde to jinak? pres Option?
-      // zrusit ten switch?
-      let preventDefault: PreventDefault = true;
-
-      // console.log(event.inputType, event.getTargetRanges());
-
       switch (event.inputType) {
-        case 'insertText':
-          if (domSelection.isCollapsed) {
-            preventDefault = insertTextOnTyping(arg);
-          } else {
-            // insertText(arg);
-          }
+        case 'insertText': {
+          insertText(event, afterTyping, dispatch, doc, getPathByDOMNode);
           break;
-
-        case 'insertReplacementText':
-          preventDefault = insertReplacementTextOnTyping(arg);
+        }
+        case 'insertReplacementText': {
+          insertReplacementText(event, afterTyping, dispatch);
           break;
-
+        }
+        // I don't understand why deleteContent needs a direction.
         case 'deleteContentBackward':
         case 'deleteContentForward': {
-          const onlyTextIsAffectedByAction =
-            domSelection.isCollapsed &&
-            // nodeValue != null for text node.
-            // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeValue
-            domSelection.anchorNode.nodeValue != null &&
-            domSelection.anchorOffset !==
-              (event.inputType === 'deleteContentBackward'
-                ? 0
-                : domSelection.anchorNode.nodeValue.length);
-          if (onlyTextIsAffectedByAction) {
-            preventDefault = deleteContentOnTyping(arg);
-          } else {
-            pipe(
-              getSelectionFromInputEvent(getPathByDOMNode, event)(),
-              fold(constVoid, selection => {
-                dispatch({ type: 'deleteContent', selection });
-              }),
-            );
-          }
+          deleteContent(event, afterTyping, dispatch, doc, getPathByDOMNode);
           break;
         }
-
-        case 'insertParagraph': {
-          // console.log('f');
-          // jo
-          break;
-        }
-
         default:
+          // Prevent unhandled until it will be handled.
           event.preventDefault();
           warn(`Unhandled beforeinput inputType: ${event.inputType}`);
       }
-
-      if (preventDefault) event.preventDefault();
     };
 
     // @ts-ignore Outdated types.
